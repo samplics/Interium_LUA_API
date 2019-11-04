4f2ab4c5fdc13cd27eea31d54c4286b08aef039a
N1ceL <41979846+N1ceL@users.noreply.github.com>
2019-11-04 23:47:01 +1000

    Add files via upload

--- HitSound.lua
+++ HitSound.lua
@@ -5,8 +5,15 @@ Menu.Spacing()
 Menu.Checkbox("Hit Sound", "bHitSound", false)
 
 local function Func(Event)
-    if Menu.GetBool("bHitSound") and Event:GetName("player_hurt", 0) == "player_hurt" then
-        PlaySound("C:/Interium/HitSound.wav")
+    if Menu.GetBool("bHitSound") == false then
+        return
+    end 
+
+    if Event:GetName("player_hurt", 0) == "player_hurt" then
+        local IsLocalShot = IEngine.GetPlayerForUserID(Event:GetInt("attacker", 0)) == IEngine.GetLocalPlayer()
+        if IsLocalShot then
+            PlaySound("C:/Interium/HitSound.wav")
+        end
     end 
 end
 
