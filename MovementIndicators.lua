3b348e967cdcd7a8010673a0e9ad77d709988a7c
N1ceL <41979846+N1ceL@users.noreply.github.com>
2019-12-31 12:10:02 +1000

    Add files via upload

--- MovementIndicators.lua
+++ MovementIndicators.lua
@@ -32,15 +32,15 @@ local function RenderSpeed()
     end
 
 	if (GetBool(Vars.misc_jumpbug) and (Globals.IsJumpBug())) then
-        Render.Text_TahomaBd("JB", (Globals.ScreenWeight()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[0]), 24, Color.new(255, 50, 50, 255), true, true)
+        Render.Text_TahomaBd("JB", (Globals.ScreenWidth()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[0]), 24, Color.new(255, 50, 50, 255), true, true)
     end
 
 	if (GetBool(Vars.misc_autostrafe) and GetInt(Vars.misc_autostrafe_enabletype) and (Globals.IsAutoStrafe())) then
-        Render.Text_TahomaBd("AS", (Globals.ScreenWeight()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[1]), 24, Color.new(50, 255, 50, 255), true, true)
+        Render.Text_TahomaBd("AS", (Globals.ScreenWidth()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[1]), 24, Color.new(50, 255, 50, 255), true, true)
     end
 
 	if (GetBool(Vars.misc_autoduck) and GetInt(Vars.misc_autoduck_enabletype) and (Globals.IsAutoDuck())) then
-        Render.Text_TahomaBd("AD", (Globals.ScreenWeight()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[2]), 24, Color.new(50, 50, 255, 255), true, true)
+        Render.Text_TahomaBd("AD", (Globals.ScreenWidth()) / 2, (Globals.ScreenHeight()) / 2 + (25 + Pos[2]), 24, Color.new(50, 50, 255, 255), true, true)
     end
 end
 
