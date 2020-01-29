87a46449de1a54ab4f4a8cf3fb96ee83df92a36e
N1ceL <41979846+N1ceL@users.noreply.github.com>
2020-01-29 16:27:48 +1000

    Add files via upload

--- example_ModelChanger.lua
+++ example_ModelChanger.lua
@@ -1,10 +1,8 @@
-local function test(pFilePath)
+function FindMDL(pFilePath)
 	local sFilePath = GetString(pFilePath)
 
-	if sFilePath:find("v_knife", 0) then
-		Print(sFilePath)
-		SetString(pFilePath, "models/weapons/v_minecraft_pickaxe.mdl")
+	if (sFilePath:find("v_knife", 0)) then -- find all viewmodel knifes
+		SetString(pFilePath, "models/weapons/v_minecraft_pickaxe.mdl") -- Set Knife Model to Minecraft Pickaxe (need model file in this path)
 	end
 end
-
-Hack.RegisterCallback("FindMDL", test)
+Hack.RegisterCallback("FindMDL", FindMDL)
