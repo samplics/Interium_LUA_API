c8d60eaa203f00c211373744dcbecea7b81efefd
N1ceL <41979846+N1ceL@users.noreply.github.com>
2020-06-21 00:10:28 +1000

    Update example_ChangeHackChams.lua

--- example_ChangeHackChams.lua
+++ example_ChangeHackChams.lua
@@ -1,2 +1,13 @@
-local MatSettings = "\"VertexlitGeneric\" { \"$additive\" \"1\" \"$envmap\" \"models/effects/cube_white\" \"$envmaptint\" \"[1 1 1]\" \"$envmapfresnel\" \"1\" \"$envmapfresnelminmaxexp\" \"[0 2 3]\" \"$alpha\" \"1\" }"
+local MatSettings = 
+[[ 
+    "VertexlitGeneric" 
+    {
+		"$additive" "1"
+		"$envmap" "models/effects/cube_white"
+		"$envmaptint" "[1 1 1]"
+		"$envmapfresnel" "1"
+		"$envmapfresnelminmaxexp" "[0 1 2]"
+		"$alpha" "1"
+    }
+]]
 IMaterialSystem.ChangeHackChams(1, "Mat1", MatSettings)
