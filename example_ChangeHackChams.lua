03a8d58cfc78f25e81152977734e24aafe9a061c
N1ceL <41979846+N1ceL@users.noreply.github.com>
2020-06-20 22:17:22 +1000

    Add files via upload

---
+++ example_ChangeHackChams.lua
@@ -0,0 +1,2 @@
+local MatSettings = "\"VertexlitGeneric\" { \"$additive\" \"1\" \"$envmap\" \"models/effects/cube_white\" \"$envmaptint\" \"[1 1 1]\" \"$envmapfresnel\" \"1\" \"$envmapfresnelminmaxexp\" \"[0 2 3]\" \"$alpha\" \"1\" }"
+IMaterialSystem.ChangeHackChams(1, "Mat1", MatSettings)
